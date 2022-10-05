7666c4e29a54625fce6a35e83fbfab775e8dad42
Othmane Filali <othmane.filali@gmail.com>
2022-10-05 10:32:01 +0200

    JS updates

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,17 @@
+const unreadMessages = document.querySelectorAll(".unread");
+const unread = document.getElementById("notifes");
+const markAll = document.getElementById("mark_all");
+unread.innerText=unreadMessages.length
 
-
-const notification = document.querySelectorAll("[data-notif]");
-const markBtn = document.querySelector ("[mark-btn]");
-
-markBtn.addEventListener('click', function () {
-    notification.classList.add('active');
-});
+unreadMessages.forEach((message) => {
+    message.addEventListener("click", () => {
+        message.classList.remove("unread");
+        const newUnreadMessages = document.querySelectorAll(".unread");
+        unread.innerText = newUnreadMessages.length;
+    })
+})
+markAll.addEventListener("click", () => {
+    unreadMessages.forEach(message => message.classList.remove("unread"))
+    const newUnreadMessages = document.querySelectorAll(".unread");
+    unread.innerText = newUnreadMessages.length;
+})
